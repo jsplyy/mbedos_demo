6d81a778eadf3fc8811c59df2a1d0b6d27383e7b
kinsey <jingsai.lu@nxp.com>
2016-08-25 20:17:53 +0800

    add yotta json

--- source/mbedos_demo.cpp
+++ source/mbedos_demo.cpp
@@ -10,23 +10,7 @@
 #include "sal/socket_api.h"
 #include "core-util/FunctionPointer.h"
 
-namespace {
-// const char *HTTP_SERVER_NAME = "ss.iotcent.org";
-const char *HTTP_SERVER_NAME = "ss.iotcent.org";
-
-const int HTTP_SERVER_PORT = 81;
-const int RECV_BUFFER_SIZE = 600;
-
-volatile char HTTP_PATH[] = "/test?temp=34";
-const size_t HTTP_PATH_LEN = sizeof(HTTP_PATH) - 1;
-
-/* Test related data */
-const char *HTTP_OK_STR = "200 OK";
-const char *HTTP_HELLO_STR = "Hello world!";
-}
-
 using namespace mbed::util;
-using namespace mbed::Sockets::v0;
 
 /* micro define */
 #define ADCR_VDD (65535U) /* Maximum value when use 16b resolution */
@@ -54,6 +38,12 @@ struct MbedClientDevice device = {
 // Instantiate the class which implements LWM2M Client API (from simpleclient.h)
 MbedClient mbed_client(device);
 
+// Set up Hardware interrupt button.
+InterruptIn obs_button(SW2);
+InterruptIn unreg_button(SW3);
+
+// LED Output
+DigitalOut led1(LED1);
 
 
 void getADC(void)
@@ -90,147 +80,138 @@ uint32_t getTemp(void)
 	return adcValue;
 }
 
-class MbedHttp
-{
+/*
+ * The Led contains one property (pattern) and a function (blink).
+ * When the function blink is executed, the pattern is read, and the LED
+ * will blink based on the pattern.
+ */
+class LedResource {
 public:
-	MbedHttp(const char * domain, const uint16_t port) :
-		_stream(SOCKET_STACK_LWIP_IPV4), _domain(domain), _port(port)
-    {
+    LedResource() {
+        // create ObjectID with metadata tag of '3201', which is 'digital output'
+        led_object = M2MInterfaceFactory::create_object("3201");
+        M2MObjectInstance* led_inst = led_object->create_object_instance();
 
-        _error = false;
-        _gothello = false;
-        _got200 = false;
-        _bpos = 0;
-        _stream.open(SOCKET_AF_INET4);
-        _stream.setOnError(TCPStream::ErrorHandler_t(this, &MbedHttp::onError));
+        // 5853 = Multi-state output
+        M2MResource* pattern_res = led_inst->create_dynamic_resource("5853", "Pattern",
+            M2MResourceInstance::STRING, false);
+        // read and write
+        pattern_res->set_operation(M2MBase::GET_PUT_ALLOWED);
+        // set initial pattern (toggle every 200ms. 7 toggles in total)
+        pattern_res->set_value((const uint8_t*)"500:500:500:500:500:500:500", 27);
+
+        // there's not really an execute LWM2M ID that matches... hmm...
+        M2MResource* led_res = led_inst->create_dynamic_resource("5850", "Blink",
+            M2MResourceInstance::OPAQUE, false);
+        // we allow executing a function here...
+        led_res->set_operation(M2MBase::POST_ALLOWED);
+        // when a POST comes in, we want to execute the led_execute_callback
+        led_res->set_execute_function(execute_callback(this, &LedResource::blink));
     }
 
-    void startTest(const char *path) {
-        /* Initialize the flags */
-        _got200 = false;
-        _gothello = false;
-        _error = false;
-        _disconnected = false;
-        /* Fill the request buffer */
-        _bpos = snprintf(_buffer, sizeof(_buffer) - 1, "GET %s HTTP/1.1\nHost: %s\n\n", path, HTTP_SERVER_NAME);
-        // _bpos = snprintf(_buffer, sizeof(_buffer) - 1, "GET %s HTTP/1.1\n", path);
-        /* Connect to the server */
-        printf("Starting DNS lookup for %s\r\n", _domain);
-        /* Resolve the domain name: */
-        socket_error_t err = _stream.resolve(_domain, TCPStream::DNSHandler_t(this, &MbedHttp::onDNS));
-        _stream.error_check(err);
+    M2MObject* get_object() {
+        return led_object;
     }
-    /**
-     * Check if the test has completed.
-     * @return Returns true if done, false otherwise.
-     */
-    bool done() {
-        return _error || (_got200 && _gothello);
+
+    void blink(void *) {
+        // read the value of 'Pattern'
+        M2MObjectInstance* inst = led_object->object_instance();
+        M2MResource* res = inst->resource("5853");
+
+        // values in mbed Client are all buffers, and we need a vector of int's
+        uint8_t* buffIn = NULL;
+        uint32_t sizeIn;
+        res->get_value(buffIn, sizeIn);
+
+        // turn the buffer into a string, and initialize a vector<int> on the heap
+        std::string s((char*)buffIn, sizeIn);
+        std::vector<uint32_t>* v = new std::vector<uint32_t>;
+        std::stringstream ss(s);
+        std::string item;
+        // our pattern is something like 500:200:500, so parse that
+        while (std::getline(ss, item, ':')) {
+            // then convert to integer, and push to the vector
+            v->push_back(atoi((const char*)item.c_str()));
+        }
+
+        output.printf("led_execute_callback pattern=%s\r\n", s.c_str());
+
+        // do_blink is called with the vector, and starting at -1
+        do_blink(v, 0);
     }
-    /**
-     * Check if there was an error
-     * @return Returns true if there was an error, false otherwise.
-     */
-    bool error() {
-        return _error;
-    }
-protected:
-    void onError(Socket *s, socket_error_t err) {
-        (void) s;
-        printf("MBED: Socket Error: %s (%d)\r\n", socket_strerror(err), err);
-        _stream.close();
-        _error = true;
-        printf("{{%s}}\r\n",(error()?"failure":"success"));
-        printf("{{end}}\r\n");
-    }
-    /**
-     * On Connect handler
-     * Sends the request which was generated in startTest
-     */
-    void onConnect(TCPStream *s) {
-        char buf[16];
-        _remoteAddr.fmtIPv4(buf,sizeof(buf));
-        printf("Connected to %s:%d\r\n", buf, _port);
-        /* Send the request */
-        s->setOnReadable(TCPStream::ReadableHandler_t(this, &MbedHttp::onReceive));
-        s->setOnDisconnect(TCPStream::DisconnectHandler_t(this, &MbedHttp::onDisconnect));
-        printf("Sending HTTP Get Request...\r\n");
-        socket_error_t err = _stream.send(_buffer, _bpos);
-        s->error_check(err);
-    }
-    /**
-     * On Receive handler
-     * Parses the response from the server, to check for the HTTP 200 status code and the expected response ("Hello World!")
-     */
-    void onReceive(Socket *s) {
-        printf("HTTP Response received.\r\n");
-        _bpos = sizeof(_buffer);
-        /* Read data out of the socket */
-        socket_error_t err = s->recv(_buffer, &_bpos);
-        if (err != SOCKET_ERROR_NONE) {
-            onError(s, err);
+
+private:
+    M2MObject* led_object;
+
+    void do_blink(std::vector<uint32_t>* pattern, uint16_t position) {
+        // blink the LED
+        led1 = !led1;
+
+        // up the position, if we reached the end of the vector
+        if (position >= pattern->size()) {
+            // free memory, and exit this function
+            delete pattern;
             return;
         }
-        _buffer[_bpos] = 0;
-        /* Check each of the flags */
-        _got200 = _got200 || strstr(_buffer, HTTP_OK_STR) != NULL;
-        _gothello = _gothello || strstr(_buffer, HTTP_HELLO_STR) != NULL;
-        /* Print status messages */
-        printf("HTTP: Received %d chars from server\r\n", _bpos);
-        printf("HTTP: Received 200 OK status ... %s\r\n", _got200 ? "[OK]" : "[FAIL]");
-        printf("HTTP: Received '%s' status ... %s\r\n", HTTP_HELLO_STR, _gothello ? "[OK]" : "[FAIL]");
-        printf("HTTP: Received message:\r\n\r\n");
-        printf("%s", _buffer);
-        _error = !(_got200 && _gothello);
 
-        s->close();
+        // how long do we need to wait before the next blink?
+        uint32_t delay_ms = pattern->at(position);
+
+        // we create a FunctionPointer to this same function
+        FunctionPointer2<void, std::vector<uint32_t>*, uint16_t> fp(this, &LedResource::do_blink);
+        // and invoke it after `delay_ms` (upping position)
+        minar::Scheduler::postCallback(fp.bind(pattern, ++position)).delay(minar::milliseconds(delay_ms));
     }
-    /**
-     * On DNS Handler
-     * Reads the address returned by DNS, then starts the connect process.
-     */
-    void onDNS(Socket *s, struct socket_addr addr, const char *domain) {
-        /* Check that the result is a valid DNS response */
-        if (socket_addr_is_any(&addr)) {
-            /* Could not find DNS entry */
-            printf("Could not find DNS entry for %s", HTTP_SERVER_NAME);
-            onError(s, SOCKET_ERROR_DNS_FAILED);
-        } else {
-            /* Start connecting to the remote host */
-            char buf[16];
-            _remoteAddr.setAddr(&addr);
-            _remoteAddr.fmtIPv4(buf,sizeof(buf));
-            printf("DNS Response Received:\r\n%s: %s\r\n", domain, buf);
-            printf("Connecting to %s:%d\r\n", buf, _port);
-            socket_error_t err = _stream.connect(_remoteAddr, _port, TCPStream::ConnectHandler_t(this, &MbedHttp::onConnect));
-
-            if (err != SOCKET_ERROR_NONE) {
-                onError(s, err);
-            }
-        }
-    }
-    void onDisconnect(TCPStream *s) {
-        s->close();
-        printf("{{%s}}\r\n",(error()?"failure":"success"));
-        printf("{{end}}\r\n");
-    }
-
-protected:
-    TCPStream _stream;              /**< The TCP Socket */
-    const char *_domain;            /**< The domain name of the HTTP server */
-    const uint16_t _port;           /**< The HTTP server port */
-    char _buffer[RECV_BUFFER_SIZE]; /**< The response buffer */
-    size_t _bpos;                   /**< The current offset in the response buffer */
-    SocketAddr _remoteAddr;         /**< The remote address */
-    volatile bool _got200;          /**< Status flag for HTTP 200 */
-    volatile bool _gothello;        /**< Status flag for finding the test string */
-    volatile bool _error;           /**< Status flag for an error */
-    volatile bool _disconnected;
-
-	~MbedHttp();
-
-	/* data */
 };
+
+/*
+ * The button contains one property (click count).
+ * When `handle_button_click` is executed, the counter updates.
+ */
+class ButtonResource {
+public:
+    ButtonResource() {
+        // create ObjectID with metadata tag of '3200', which is 'digital input'
+        btn_object = M2MInterfaceFactory::create_object("3200");
+        M2MObjectInstance* btn_inst = btn_object->create_object_instance();
+        // create resource with ID '5501', which is digital input counter
+        M2MResource* btn_res = btn_inst->create_dynamic_resource("5501", "Button",
+            M2MResourceInstance::INTEGER, true /* observable */);
+        // we can read this value
+        btn_res->set_operation(M2MBase::GET_ALLOWED);
+        // set initial value (all values in mbed Client are buffers)
+        // to be able to read this data easily in the Connector console, we'll use a string
+        btn_res->set_value((uint8_t*)"0", 1);
+    }
+
+    M2MObject* get_object() {
+        return btn_object;
+    }
+
+    /*
+     * When you press the button, we read the current value of the click counter
+     * from mbed Device Connector, then up the value with one.
+     */
+    void handle_button_click() {
+        M2MObjectInstance* inst = btn_object->object_instance();
+        M2MResource* res = inst->resource("5501");
+
+        // up counter
+        counter++;
+
+        printf("handle_button_click, new value of counter is %d\r\n", counter);
+
+        // serialize the value of counter as a string, and tell connector
+        stringstream ss;
+        ss << counter;
+        std::string stringified = ss.str();
+        res->set_value((uint8_t*)stringified.c_str(), stringified.length());
+    }
+
+private:
+    M2MObject* btn_object;
+    uint16_t counter = 0;
+};  
 
 class TempResource
 {
@@ -285,24 +266,6 @@ private:
 	/* data */
 };
 
-MbedHttp* mbedHttp;
-void push()
-{
-	static uint32_t count=0;
-	uint32_t wendu = getTemp();
-	count++;
-	HTTP_PATH[HTTP_PATH_LEN-1] = wendu%10+48;
-	HTTP_PATH[HTTP_PATH_LEN-2] = wendu/10%10+48;
-
-	printf("count=%ld HTTP_PATH:%s\n", count, HTTP_PATH);
-
-	mbedHttp = new MbedHttp(HTTP_SERVER_NAME, HTTP_SERVER_PORT);
-
-	mbed::util::FunctionPointer1<void, const char*> fp1(mbedHttp, &MbedHttp::startTest);
-
-	minar::Scheduler::postCallback(fp1.bind((const char*)HTTP_PATH));
-
-}
 void app_start(int, char**)
 {
 	output.baud(115200);
@@ -319,44 +282,34 @@ void app_start(int, char**)
 	}
 	output.printf("IPv4 address is:%s\r\n", eth.getIPAddress());
 	output.printf("endpoint device name is:%s\r\n", MBED_ENDPOINT_NAME);
-    
-//section  mbed http
- //    /* mbed http desperate from mbed connector server */
-	// mbedHttp = new MbedHttp(HTTP_SERVER_NAME, HTTP_SERVER_PORT);
- //    minar::Scheduler::postCallback(push).period(minar::milliseconds(5000));
-//section mbed http
 
     auto tempRes = new TempResource();
-    mbed_client.create_interface();
-    // server = new TcpSocketServer;
+    // we create our button and LED resources
+    auto button_resource = new ButtonResource();
+    auto led_resource = new LedResource();
 
+    // Unregister button (SW3) press will unregister endpoint from connector.mbed.com
+    unreg_button.fall(&mbed_client, &MbedClient::test_unregister);
+
+    // Observation Button (SW2) press will send update of endpoint resource values to connector
+    obs_button.fall(button_resource, &ButtonResource::handle_button_click);
+
+    mbed_client.create_interface();
 
 	M2MSecurity* registerObject = mbed_client.create_register_object();
 	M2MDevice* deviceObject = mbed_client.create_device_object();
 
 	M2MObjectList objectList;
 	objectList.push_back(deviceObject);
+    objectList.push_back(button_resource->get_object());
+    objectList.push_back(led_resource->get_object());
 	objectList.push_back(tempRes->getObject());
-	// objectList.push_back(tempRes->getADC());
 
 	mbed_client.set_register_object(registerObject);
 
 	FunctionPointer2<void, M2MSecurity*, M2MObjectList> fp(&mbed_client, &MbedClient::test_register);
-	// mbed::util::FunctionPointer1<void, uint16_t> fp1(server, &TcpSocketServer::start);
-	// minar::Scheduler::postCallback(fp1.bind(TCP_SERVER_PORT));
-
 
 	minar::Scheduler::postCallback(fp.bind(registerObject,objectList));
 	minar::Scheduler::postCallback(&mbed_client,&MbedClient::test_update_register).period(minar::milliseconds(25000));
-	// // minar::Scheduler::postCallback(adc).period(minar::milliseconds(500));
-	// // minar::Scheduler::postCallback().period(minar::milliseconds(500));
-	// // minar::Scheduler::postCallback(getADC).period(minar::milliseconds(500));
-	// mbed::util::FunctionPointer1<void, const char*> fp1(mbedHttp, &MbedHttp::startTest);
-	// mbed::util::FunctionPointer1<void, const char*> fp1(mbedHttp, &MbedHttp::startTest);
-
-	// minar::Scheduler::postCallback(fp1.bind(HTTP_PATH));
-
-	// minar::Scheduler::postCallback(fp1.bind(HTTP_PATH)).period(minar::milliseconds(5000));
-
 
 }
